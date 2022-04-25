e45e61f2e8e89f74d9014b0fd59f6c8458da731a
sapar6ek0v <razetka0309@gmail.com>
2022-04-25 12:34:19 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-import {Route, Routes} from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import './App.css'
+import Main from './pages/Main/Main';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-    return (
-        <div className='wrapper'>
-            <div className='main'>
-                <Header/>
-                <Routes>
-                    <Route path='/' element={<Main/>}/>
-                </Routes>
-            </div>
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
 
-            <Footer />
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
